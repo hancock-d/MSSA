ded77855249a061bb53f344f4120264e8d69a997
Dave Hancock <dthancock4@gmail.com>
2025-04-10 10:54:35 -0700

    Delete MonkeyFinder v1

--- MAUI/MonkeyFinder/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using MonkeyFinder.Data;
-global using MonkeyFinder.PageModels;
-global using MonkeyFinder.Pages;
-global using MonkeyFinder.Services;
-global using MonkeyFinder.Utilities;
