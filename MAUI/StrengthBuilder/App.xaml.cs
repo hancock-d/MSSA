c59e91b7b814df6c3de5f754922fb09819ca9976
Dave Hancock <dthancock4@gmail.com>
2025-04-21 12:19:02 -0700

    Adding StrengthBuilder

---
+++ MAUI/StrengthBuilder/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace StrengthBuilder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
